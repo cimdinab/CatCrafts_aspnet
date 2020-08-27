bc289dc7d6662c8c821a0672758d099945dfe3d9
Barba Ilva Cimdiņa <cimdina.barba@gmail.com>
2020-08-27 23:39:51 +0300

    Added data to Razor page

--- CatCrafts.WebSite/Pages/Index.cshtml.cs
+++ CatCrafts.WebSite/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CatCrafts.WebSite.Models;
+using CatCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,19 @@ namespace CatCrafts.WebSite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileCatsService CatService;
+        public IEnumerable<Cat> Cats { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileCatsService catService)
         {
             _logger = logger;
+            CatService = catService;
         }
 
         public void OnGet()
         {
-
+            Cats = CatService.GetCats();
         }
     }
 }
---
+++ CatCrafts.WebSite/Services/JsonFileCatsService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using CatCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CatCrafts.WebSite.Services
+{
+    public class JsonFileCatsService
+    {
+        public JsonFileCatsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "cats.json"); }
+        }
+
+        public IEnumerable<Cat> GetCats()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Cat[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+
+}
